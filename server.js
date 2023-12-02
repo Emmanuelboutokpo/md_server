d8ab24c398a744e4c89708347b80bb2ea90d8f5e
emmanuel19996 <emmanuel.boutokpo@africadigitalizer.com>
2023-12-02 19:09:54 +0100

    Backend Md archive finaly

--- server.js
+++ server.js
@@ -7,13 +7,18 @@ const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 const cors =require("cors")
 
-app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
 app.use(logger);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/', express.static(path.join(__dirname, 'uploads')));
-
-
+  
 const users = require('./routes/userRoutes');
 const gender = require('./routes/genderRoutes');
 const music = require('./routes/music');
