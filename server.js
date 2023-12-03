8d97ba4bfefa93ac2d96a8412f10ccf1f4ad36c6
emmanuel19996 <emmanuel.boutokpo@africadigitalizer.com>
2023-12-03 18:43:58 +0100

    Backend Md archive finaly

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(logger);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/', express.static(path.join(__dirname, 'uploads')));
-app.use('/api', createProxyMiddleware({ target: 'https://md-archive.onrender.com', changeOrigin: true }));
+app.use('/api', createProxyMiddleware({ target: 'https://mdarchive.onrender.com', changeOrigin: true }));
 
 
 const users = require('./routes/userRoutes');
