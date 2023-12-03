c848b98c755bb2d8164b316a31d9c5842cc32406
emmanuel19996 <emmanuel.boutokpo@africadigitalizer.com>
2023-12-03 18:35:52 +0100

    Backend Md archive finaly

--- server.js
+++ server.js
@@ -5,20 +5,16 @@ const app = express();
 const path = require('path');
 const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
-const cors =require("cors")
+//const cors =require("cors")
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-app.use(cors());
+//app.use(cors());
 app.use(logger);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/', express.static(path.join(__dirname, 'uploads')));
+app.use('/api', createProxyMiddleware({ target: 'https://md-archive.onrender.com', changeOrigin: true }));
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-    next();
-});
 
 const users = require('./routes/userRoutes');
 const gender = require('./routes/genderRoutes');
@@ -27,6 +23,7 @@ const music = require('./routes/music');
 app.use('/api', users);
 app.use('/api', gender);
 app.use('/api', music);
+
 
 app.all('*', (req, res) => {
     res.status(404);
