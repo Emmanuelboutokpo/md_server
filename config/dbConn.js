e06ec6b12a8b06118cd8f898852bd11269a29d27
emmanuel19996 <emmanuel.boutokpo@africadigitalizer.com>
2023-12-04 11:12:14 +0100

    Backend Md archive finaly

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createPool({ 
-  connectionLimit:10,
+  connectionLimit:100,
   host  : process.env.DB_HOST,
   user  : process.env.DB_USER,
   password : process.env.DB_PASSWORD,
